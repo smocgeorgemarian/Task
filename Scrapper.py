0d552a67aab4d1c3a77e894e6e33320bd376bfbf
smocgeorgemarian <george.smoc13@gmail.com>
2023-02-08 12:03:05 +0200

    First commit

---
+++ Scrapper.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+class Scrapper:
+    def __init__(self, URL):
+        self.URL = URL
+        self.driver = webdriver.
+
+    def parse_data(self):
---
+++ main.py
@@ -0,0 +1,13 @@
+from Scrapper import Scrapper
+
+
+def main():
+    scrapper = Scrapper("https://samples.vx-underground.org/samples/Families/")
+    scrapper.parse_data()
+
+
+
+if __name__ == '__main__':
+    main()
+
+
