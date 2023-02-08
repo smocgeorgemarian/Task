756b455ea6da4ca60c5f34d1744219bed22d3be7
smocgeorgemarian <george.smoc13@gmail.com>
2023-02-08 12:20:47 +0200

    Getting the table of families

--- Scrapper.py
+++ Scrapper.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
 class Scrapper:
     def __init__(self, URL):
         self.URL = URL
-        self.driver = webdriver.
+        self.driver = webdriver.Firefox()
 
     def parse_data(self):
+        self.driver.get(self.URL)
+        table = self.driver.find_element(By.XPATH, '/html/body/table/tbody')
+
+        print(table)
+        self.driver.quit()
+
